39726dbca3fd6f333984d115fec026d0a682ff95
Noam <noambires@gmail.com>
2021-10-24 11:51:26 +0200

    new class

---
+++ CentreSPAPi.java
@@ -0,0 +1,52 @@
+public class CentreSPAPi {
+
+    public class CentreSPATab {
+
+        private int capaMax;
+        private Animal[] cage;
+
+        public CentreSPATab(int capaMax) {
+            this.capaMax = capaMax;
+            cage = new Animal[capaMax];
+        }
+
+        // index out 0 of bound car tableau length 0
+        public void entrer(Animal a) {
+            // A completer
+        }
+
+        public String toString() {
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < capaMax; i++) {
+                if (cage[i] != null) {
+                    String a = "case #" + i + " Animal " + cage[i].getEspece() + " Numéros de Puce : "
+                            + cage[i].getNumPuce() + "\n";
+                    sb.append(a);
+                }
+            }
+            return sb.toString();
+        }
+
+        public boolean sortir(Animal a) {
+            for (int i = 0; i < capaMax; i++) {
+                if (cage[i] == a) {
+                    cage[i] = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public boolean sortir(int a) {
+            for (int i = 0; i < capaMax; i++) {
+                if (cage[i].getNumPuce() == a) {
+                    cage[i] = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
